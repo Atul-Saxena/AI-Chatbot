156c44be477d8b870869dd9c34c5a377e381c158
Atul Saxena <atulsaxena194.com>
2024-12-21 18:49:59 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,51 @@
+import "dotenv/config";
+import express from "express";
+import OpenAI from "openai";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+
+app.post("/", async(req, res) => {
+    const baseURL = process.env.BASE_URL;
+    const apiKey = process.env.API_KEY;
+    const userPrompt = req.body.data;
+    const systemPrompt = "You are a blockchain expert. Keep it simple and very short and always be helpful. Be descriptive when necessary only";
+    
+
+    const api = new OpenAI({
+        apiKey,
+        baseURL,
+    });
+
+    const completion = await api.chat.completions.create({
+        model: "mistralai/Mistral-7B-Instruct-v0.2",
+        messages: [
+            {
+                role: "system",
+                content: systemPrompt,
+            },
+            {
+                role: "user",
+                content: userPrompt,
+            },
+        ],
+        temperature: 0.7,
+        max_tokens: 256,
+    });
+
+    const response = completion.choices[0].message.content;
+    console.log("User:", userPrompt);
+    console.log("AI:", response);
+
+    res.send(response);
+});
+
+app.listen(3000, () => {
+    console.log("Example app listening on port 3000!");
+});
+
